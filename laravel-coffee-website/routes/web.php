84ebe9d6d7b7ede50a750b2d51521c4eb0150928
BangHann <hibatullahfarhan4@gmail.com>
2024-03-03 10:55:19 +0700

    nambah data

--- laravel-coffee-website/routes/web.php
+++ laravel-coffee-website/routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/kopi_index', [KopiController::class, 'index']);
+Route::get('/index', [KopiController::class, 'index']);
