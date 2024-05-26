86c8724aba2d73ab5506571e8beaacff5b32f7d7
alaric2001 <alaric2001ra@gmail.com>
2024-05-26 21:24:19 +0700

    Update: Light theme Profile

--- laravel-coffee-website/resources/views/profile/edit_raw.blade.php
+++ laravel-coffee-website/resources/views/profile/edit_raw.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Profile') }} {{ Auth::user()->name_user }}
         </h2>
     </x-slot>
@@ -13,19 +13,19 @@
                 <button type="submit" class="w-full py-2 bg-[#baa785] hover:bg-[#82755d] hover:text-[#FFE5B6] rounded-md">Logout</button>
             </form>
 
-            <div class="p-4 sm:p-8 bg-white  shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white  shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white  shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
