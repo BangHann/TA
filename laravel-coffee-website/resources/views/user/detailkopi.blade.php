82fbaad3ab07edc0ca89e252705f6082d6b638aa
alaric2001 <alaric2001ra@gmail.com>
2024-07-05 14:25:07 +0700

    Update: Detail Kopi

--- laravel-coffee-website/resources/views/user/detailkopi.blade.php
+++ laravel-coffee-website/resources/views/user/detailkopi.blade.php
@@ -18,7 +18,7 @@
                 <p>Stock {{ $detail_kopi->stok }}</p>
             </div> --}}
             @if($data_rasa->isNotEmpty())
-                <p class="font-semibold pt-4">Rasa</p>
+                <p class="font-semibold pt-4">Jenis Kopi</p>
                 <div class="">
                     @foreach ($data_rasa as $item)
                         <button type="button" class="rasa-button mr-1 mt-1 text-xs text-secondary bg-primary rounded-md p-2 hover:bg-primary_hover active:bg-secondary active:text-primary" data-id="{{ $item->id }}">
